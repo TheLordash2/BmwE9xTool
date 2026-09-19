482e0f09de2d38be6e360cb835d810d66d64eef3
TheLordash2 <135472436+TheLordash2@users.noreply.github.com>
2026-09-19 15:45:54 +0300

    chore: remove obsolete Kotlin prototype

--- app/src/main/java/com/thelordash2/bmwe9xtool/transport/BmwTransport.kt
+++
@@ -1,8 +0,0 @@
-package com.thelordash2.bmwe9xtool.transport
-
-interface BmwTransport {
-    val connected: Boolean
-    suspend fun connect(): Result<Unit>
-    suspend fun readVin(): Result<String>
-    suspend fun disconnect()
-}
